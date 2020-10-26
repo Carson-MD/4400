688e4b9faff8133eebefd32d8aa7dec3998415de
Carson-MD <carsonmdickie@gmail.com>
2020-10-26 12:06:52 -0400

    Update database.pl
    Added documentation.

--- database.pl
+++ database.pl
@@ -9,8 +9,36 @@ associated(variant, drug, pubID, adjective (inc-dec), 'attribute').
 BRAINSTORM:
 bestMed(Allele, illness, Med, pubId):- treats(Med, illness),positivelyassociated(Allele, Med, pubId).
 wrstMed(Allele, illness, Med, pubId):- treats(Med, illness),negativelyassociated(Allele, Med, pubId).
-associated(Allele, Med, pubId, IncDec, Param)..
+associated(Allele, Med, pubId, IncDec, Param).
 
+HOW TO USE (SO FAR):
+
+1) Check if drug is in the database:
+drug(amitriptyline).
+>>return TRUE.
+drug(X).
+>>return all drugs in the database.
+
+2) Check if illness is in the database:
+illness(Depression).
+>>return TRUE.
+illness(X).
+>>return all illnesses in the database.
+
+3) Check for associations:
+FORMAT
+associated(gene_variant, drug, pubmed_ID, increased_decreased, details).
+gene_variant - the gene variant of interest
+drug - the drug of interest
+pubmed_ID - the pubmed ID for the paper in which the association was identified
+increased_decreased - the direction of the association
+details - the details of the association
+EXAMPLE
+associated('CYP2C19*1/*2', amitriptyline, X, Y, Z).
+>>X = 15590749
+  Y = increased
+  Z = 'concentration vs CYP2C19*1/*1'
+  ...(plus additional matches)
 *******************************************************************************/
 
 /********
